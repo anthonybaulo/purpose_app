e46e18a70053b33645c9363536d4fc5583cfcb0f
Anthony <myworldofsand@gmail.com>
2020-06-12 14:11:43 -0500

    Create my users_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+
   devise_for :users
+  resources :users, :only => [:index, :show, :destroy]
   root 'static_pages#home'
   get 'static_pages/help'
   get 'static_pages/about'
-  get 'static_pages/contact'  
+  get 'static_pages/contact'
+  # get 'users/index'
+  # get 'users/show'
+  # get 'users/destroy'    
 end
