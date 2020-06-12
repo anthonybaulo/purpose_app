03780d8bc0afc86ffc1bb428b515c1bbb4a15df5
Anthony <myworldofsand@gmail.com>
2020-06-11 21:19:06 -0500

    Create MissionStatement model and associations with User

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup 
+    @user = User.new(email: "test@example.com", password: "foobar", 
+                     password_confirmation: "foobar") 
+  end
+  
+  test "associated mission_statements should be destroyed" do 
+    @user.save 
+    @user.mission_statements.create!(content: "Lorem ipsum") 
+    assert_difference 'MissionStatement.count', -1 do 
+      @user.destroy 
+    end 
+  end
 end
