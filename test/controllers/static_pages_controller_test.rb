2df9b80c0b8ab45788d8f32e6d2a20c1fc257d2d
Anthony <myworldofsand@gmail.com>
2020-06-11 21:37:23 -0500

    Update static_pages_controller_test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,28 +6,18 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title = "Purpose App"
   end
 
-  test "should get root" do
+  test "should get root without sign in" do
     get root_url
     assert_response :success
     assert_select "title", "Home | #{@base_title}"
   end
 
-  test "should get help" do
-    get static_pages_help_url
-    assert_response :success
-    assert_select "title", "Help | #{@base_title}"
-  end
-
-  test "should get about" do
-    get static_pages_about_url
-    assert_response :success
-    assert_select "title", "About | #{@base_title}"
-  end
-
-  test "should get contact" do
-    get static_pages_contact_url
-    assert_response :success
-    assert_select "title", "Contact | #{@base_title}"
+  test "should redirect without sign in" do
+    paths = [static_pages_help_url, static_pages_about_url, static_pages_contact_url]
+    paths.each do |path|
+      get path
+      assert_redirected_to new_user_session_path
+    end
   end
 end
 
