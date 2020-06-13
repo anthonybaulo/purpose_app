5d28652985eb709ad266b99ac7247a48bf841aca
Anthony <myworldofsand@gmail.com>
2020-06-13 18:03:30 -0500

    Redirect user after sign in

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,7 @@ class StaticPagesController < ApplicationController
   skip_before_action :authenticate_user!, :only => [:home]
 
   def home
+    redirect_to @current_user if user_signed_in?
   end
 
   def help
