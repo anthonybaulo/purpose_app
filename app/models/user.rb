53b734a9a13b7f022d44359a7b26d3d280976ec8
Anthony <myworldofsand@gmail.com>
2020-06-19 00:06:33 -0500

    Initialize SendGrid

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :omniauthable, :confirmable, :lockable, :timeoutable, :trackable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, 
-         :validatable
+         :validatable, :confirmable, :lockable, :timeoutable
   has_many :mission_statements, dependent: :destroy
 end
