1873388d37210b3ab7468084d1cb3d8f1fc4a569
Anthony <myworldofsand@gmail.com>
2020-06-27 19:25:39 -0500

    Set up follow relationships

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-# Create a main sample user
+# Create a main sample user.
 # The ! will raise an exception if it doesn't work (noisy method)
 User.create!(email:                 "main@example.com",
              password:              "anthony",
@@ -22,17 +14,33 @@ User.create!(email:                 "main@example.com",
                password_confirmation: password)
 end
 
-# Create 3 MS's for second user
+# Create 3 MS's for second user.
 user2 = User.second
 3.times do
   content = Faker::Lorem.sentence(word_count: 40)
   user2.mission_statements.create!(content: content)
 end
 
-# Create single Mission Statement for all but first 2 users
+# Create single Mission Statement for all but first 2 users.
 users = User.all
 
 users[2..-1].each do |user| 
   content = Faker::Lorem.sentence(word_count: 40)
   user.mission_statements.create!(content: content)
 end
+
+# Create Posts for users.
+3.times do
+  users.each do |user| 
+    title = Faker::Company.bs
+    content = Faker::Lorem.sentence(word_count: 30)
+    user.posts.create!(title: title, content: content)
+  end
+end
+
+# Create following relationships.
+user  = users.first
+following = users[2..40]
+followers = users[10..30]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
