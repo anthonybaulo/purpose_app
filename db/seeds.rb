dd6f98a4d2ad4b46cd11474223414eeab953ebd6
Anthony <myworldofsand@gmail.com>
2020-06-29 10:51:03 -0500

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,12 @@
 # Create a main sample user.
 # The ! will raise an exception if it doesn't work (noisy method)
-User.create!(email:                 "main@example.com",
-             password:              "anthony",
-             password_confirmation: "anthony")
+user = User.new(email:                 "main@example.com",
+                password:              ENV['DEV_PW'],
+                password_confirmation: ENV['DEV_PW'])
+user.skip_confirmation!
+user.save!
 
-# Generate a bunch of additional users.
+# Generate additional users.
 49.times do |n|
   name     = Faker::Name.first_name
   email    = "#{name}-#{n}@example.com"
@@ -14,32 +16,25 @@ User.create!(email:                 "main@example.com",
                password_confirmation: password)
 end
 
-# Create 3 MS's for second user.
-user2 = User.second
-3.times do
-  content = Faker::Lorem.sentence(word_count: 40)
-  user2.mission_statements.create!(content: content)
-end
-
-# Create single Mission Statement for all but first 2 users.
+# Create Mission Statement for all users.
 users = User.all
 
-users[2..-1].each do |user| 
-  content = Faker::Lorem.sentence(word_count: 40)
+users.each do |user| 
+  content = Faker::Lorem.paragraphs(number: 5, supplemental: true).join("\n\n")
   user.mission_statements.create!(content: content)
 end
 
 # Create Posts for users.
 3.times do
   users.each do |user| 
-    title = Faker::Company.bs
-    content = Faker::Lorem.sentence(word_count: 30)
+    title   = Faker::Company.bs
+    content = Faker::Lorem.paragraphs(number: 5, supplemental: true).join("\n\n")
     user.posts.create!(title: title, content: content)
   end
 end
 
 # Create following relationships.
-user  = users.first
+user      = users.first
 following = users[2..40]
 followers = users[10..30]
 following.each { |followed| user.follow(followed) }
