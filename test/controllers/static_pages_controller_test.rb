00ce4b367c9f7089de539a7c1facff5583f0cce5
Anthony <myworldofsand@gmail.com>
2020-06-30 23:55:48 -0500

    Allow static pages without sign in

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,21 +2,15 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
-  def setup
-    @base_title = "Purpose App"
-  end
+  # def setup
+  #   @base_title = "Purpose App"
+  # end
 
-  test "should get root without sign in" do
-    get root_url
-    assert_response :success
-    assert_select "title", "Home | #{@base_title}"
-  end
-
-  test "should redirect without sign in" do
-    paths = [help_path, about_path, contact_path]
+  test "should get static pages without sign in" do
+    paths = [root_url, help_path, about_path, contact_path]
     paths.each do |path|
       get path
-      assert_redirected_to new_user_session_path
+      assert_response :success
     end
   end
 end
